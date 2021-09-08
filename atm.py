3a81f91643ccada86f2d2cf900d084587df62a8a
poonamng <90316005+poonamng@users.noreply.github.com>
2021-09-09 01:00:56 +0530

    Add files via upload

---
+++ atm.py
@@ -0,0 +1,44 @@
+# print(8%5//3/4+(5**3)+5*4-9)
+# year=int(input("enter the year"))
+# if year%4==0:
+#     print("leap year")
+#     if year%400==0:
+#         print("leap year hai")     
+#     else:        
+#        print("nothing")
+# else:
+#     print("nhi h")
+
+# atm_card=input("enter type of card:")
+# if atm_card=="credit card" or "debit card":
+#     print("atm_card")
+#     language=input("choose the language:")
+#     if language=="hindi" or language=="english":
+#         print("language")
+#         pin=int(input("enter the pin 4 digit:"))
+#         if pin=="4 digit" or "6 digit":
+#             print("pin")
+#             option=input("enter your option:")
+#             if option=="withdrawal cash"or option=="balance enquary":
+#                 print("option")
+#                 account=input("choose your account:")
+#                 if account=="saving" or account=="current":
+#                     print("account")
+#                     amount=int(input("enter your amount:"))
+#                     if amount>2000:
+#                         print("you can take")
+#                     else:
+#                         print("no balance")
+#                 else:
+#                     print(" other")
+#             else:
+#                 print("other option")
+#         else:
+#             print("invali pin")
+#     else:
+#         print("other language")
+# else:
+#     print("invalid")                                
+
+
+
